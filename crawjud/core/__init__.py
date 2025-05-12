55ca8e108b5f3fb400e41629d3fbfe90d627ccd4
Robotz213 <nicholas@robotz.dev>
2025-05-12 19:01:35 -0300

    Refactor: Organize imports and enhance type hinting in CrawJUD core module

--- crawjud/core/__init__.py
+++ crawjud/core/__init__.py
@@ -1,6 +1,15 @@
 """Módulo núcleo de controle dos robôs."""
 
+from __future__ import annotations
+
 from time import perf_counter
+from typing import TYPE_CHECKING
+
+from crawjud.addons.search import SearchBot
+from crawjud.addons.webdriver import DriverBot
+
+if TYPE_CHECKING:
+    from selenium.webdriver.remote.webdriver import WebDriver
 
 
 class CrawJUD:
@@ -12,7 +21,14 @@ class CrawJUD:
 
     bot_data: dict[str, str]
     start_time: float
+    driver: WebDriver
+    search: SearchBot
 
     def __init__(self, *args: str, **kwargs: str) -> None:
         """Inicializador do núcleo."""
         self.start_time = perf_counter()
+        driverbot = DriverBot(kwargs.get("preferred_browser", "chrome"))()
+        self.driver = driverbot[0]
+        self.wait = driverbot[1]
+
+        # self.search = SearchBot.setup()
