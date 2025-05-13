c9f101d9d7e63fba3dd99b3b920c514c2383e683
Robotz213 <nicholas@robotz.dev>
2025-05-13 12:26:04 -0300

    feat: Implement logging system with Redis and file handlers
    - Added logging module in `crawjud/addons/logger/__init__.py` with configuration for file and Redis handlers.
    - Created `crawjud/addons/logger/handlers.py` for custom logging handlers to send logs to Redis and files.
    - Developed `crawjud/addons/logger/formatters.py` for JSON formatting of log records.
    - Updated `pyproject.toml` to include Redis dependency.
    - Added environment variables documentation in `celery_app/ENVIRONMENT-VARIABLES.md` for Google Cloud Storage and MailClient.

---
+++ crawjud/addons/logger/__init__.py
@@ -0,0 +1,56 @@
+"""Módulo de gerenciamento de logs do CrawJUD."""
+
+import logging
+from typing import Any
+
+handlers = {
+    "file_handler": {
+        "class": "crawjud.logger.handlers.FileHandler",
+        "level": logging.INFO,
+        "formatter": "json",
+        "filename": "app.log",
+        "maxBytes": 1024,
+        "backupCount": 1,
+    },
+    "redis_handler": {
+        "class": "crawjud.logger.handlers.RedisHandler",
+        "level": logging.INFO,
+        "formatter": "json",
+    },
+}
+
+formatters = (
+    {
+        "default": {
+            "format": "%(levelname)s:%(name)s:%(message)s",
+        },
+        "json": {
+            "()": "crawjud.logger.formatters.JsonFormatter",
+        },
+    },
+)
+
+
+def dict_config(**kwargs: str | int) -> tuple[dict[str, Any], str]:
+    """Gerador de configurações do logging."""
+    log_level: int = kwargs.get("LOG_LEVEL", logging.INFO)
+    logger_name: str = kwargs.get("LOGGER_NAME", __name__)
+    config = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "root": {
+            "level": log_level,
+            "handlers": list(handlers.keys()),
+        },
+        "handlers": handlers,
+        "formatters": formatters,
+        "loggers": {
+            logger_name: {
+                "level": log_level,
+                "handlers": list(handlers.keys()),
+                "propagate": False,
+            },
+        },
+    }
+
+    return config, logger_name
