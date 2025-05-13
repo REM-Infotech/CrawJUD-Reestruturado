756d59449a15bbf9d6043560182b9fb6ff58e7be
Robotz213 <nicholas@robotz.dev>
2025-05-13 18:17:09 -0300

    refactor: Improve logging configuration and handlers
    celery_app/__init__.py:
    - Added a new logger configuration function to customize Celery logger settings based on environment variables.
    
    celery_app/addons/logger/__init__.py:
    - Updated logging handlers configuration to use a new structure and improved file handling settings.
    
    celery_app/addons/logger/handlers/__init__.py:
    - Reintroduced custom logging handlers for Redis and file logging with enhanced functionality.
    - Removed deprecated logging handler and formatter classes.

--- celery_app/__init__.py
+++ celery_app/__init__.py
@@ -1,9 +1,57 @@
 """Módulo Celery App do CrawJUD Automatização."""
 
-from celery import Celery
-from kombu import Queue  # noqa: F401
+import logging
+from logging.config import dictConfig
+from os import getenv
+from pathlib import Path
+from typing import AnyStr
 
+from celery import Celery
+from celery.signals import after_setup_logger
+
+from celery_app.addons import worker_name_generator
+from celery_app.addons.logger import dict_config
 from celery_app.resources.load_config import Config
+
+
+@after_setup_logger.connect
+def config_loggers(
+    logger: logging.Logger,
+    *args: AnyStr,
+    **kwargs: AnyStr,
+) -> None:
+    """Configure and alter the Celery logger for the application.
+
+    This function updates the Celery logger configuration based on environment
+    variables and custom logging settings. It ensures that the Celery logger uses
+    the desired log level and handlers derived from the application's logging configuration.
+
+    Args:
+        logger (logging.Logger): The logger instance to configure.
+        *args (AnyType): Positional arguments.
+        **kwargs (AnyType): Keyword arguments, may include a 'logger' instance to be configured.
+
+    """
+    logger_name = f"{worker_name_generator()}_celery"
+    log_path = Path().cwd().joinpath("temp", "logs")
+    log_path.mkdir(exist_ok=True, parents=True)
+    log_file = log_path.joinpath(f"{logger_name}.log")
+    log_file.touch(exist_ok=True)
+
+    log_level = logging.INFO
+    if getenv("DEBUG", "False").lower() == "true":
+        log_level = logging.DEBUG
+
+    config, _ = dict_config(LOG_LEVEL=log_level, LOGGER_NAME=logger_name, FILELOG_PATH=log_file)
+
+    dictConfig(config)
+    # Alter the Celery logger using the provided logger from kwargs if available.
+    logger.setLevel(log_level)
+    # Clear existing handlers and add the ones from the new configuration.
+    logger.handlers.clear()
+    configured_logger = logging.getLogger(logger_name.replace("_", "."))
+    for handler in configured_logger.handlers:
+        logger.addHandler(handler)
 
 
 def make_celery() -> Celery:
--- celery_app/addons/logger/__init__.py
+++ celery_app/addons/logger/__init__.py
@@ -1,53 +1,54 @@
-"""Módulo de gerenciamento de logs do CrawJUD."""
+"""Módulo de gerenciamento de logs do celery_app."""
 
 import logging
 from typing import Any
-
-handlers = {
-    "file_handler": {
-        "class": "crawjud.addons.logger.handlers.FileHandler",
-        "level": logging.INFO,
-        "formatter": "json",
-        "filename": "app.log",
-        "maxBytes": 1024,
-        "backupCount": 1,
-    },
-    "redis_handler": {
-        "class": "crawjud.addons.logger.handlers.RedisHandler",
-        "level": logging.INFO,
-        "formatter": "json",
-    },
-}
-
-formatters = (
-    {
-        "default": {
-            "format": "%(levelname)s:%(name)s:%(message)s",
-        },
-        "json": {
-            "()": "crawjud.addons.logger.formatters.JsonFormatter",
-        },
-    },
-)
 
 
 def dict_config(**kwargs: str | int) -> tuple[dict[str, Any], str]:
     """Gerador de configurações do logging."""
     log_level: int = kwargs.get("LOG_LEVEL", logging.INFO)
     logger_name: str = kwargs.get("LOGGER_NAME", __name__)
+
+    handlers_config = {
+        "file_handler": {
+            "class": "celery_app.addons.logger.handlers.FileHandler",
+            "level": logging.INFO,
+            "formatter": "json",
+            "filename": "app.log",
+            "maxBytes": 1024,
+            "backupCount": 1,
+        },
+        # "redis_handler": {
+        #     "class": "celery_app.addons.logger.handlers.RedisHandler",
+        #     "level": logging.INFO,
+        #     "formatter": "json",
+        # },
+    }
+    handlers_config["file_handler"]["level"] = log_level
+    handlers_config["file_handler"]["maxBytes"] = 40960
+    handlers_config["file_handler"]["backupCount"] = 5
+    handlers_config["file_handler"]["filename"] = kwargs["FILELOG_PATH"]
+
     config = {
         "version": 1,
         "disable_existing_loggers": False,
         "root": {
             "level": log_level,
-            "handlers": list(handlers.keys()),
+            "handlers": list(handlers_config.keys()),
         },
-        "handlers": handlers,
-        "formatters": formatters,
+        "handlers": handlers_config,
+        "formatters": {
+            "default": {
+                "format": "%(levelname)s:%(name)s:%(message)s",
+            },
+            "json": {
+                "()": "celery_app.addons.logger.handlers.JsonFormatter",
+            },
+        },
         "loggers": {
             logger_name: {
                 "level": log_level,
-                "handlers": list(handlers.keys()),
+                "handlers": list(handlers_config.keys()),
                 "propagate": False,
             },
         },
