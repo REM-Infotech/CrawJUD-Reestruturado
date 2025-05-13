dac29933d93831fc18335653e2fb0a5ce18a4563
Robotz213 <nicholas@robotz.dev>
2025-05-13 15:33:33 -0300

    refactor: Simplify type annotations in configuration
    quartconf.py: Removed type annotations for configuration variables to enhance readability and maintain consistency.

--- api/quartconf.py
+++ api/quartconf.py
@@ -9,46 +9,46 @@ workdir = Path(__file__).cwd()
 
 WITH_REDIS = False
 LOG_LEVEL = logging.INFO
-DEBUG: type[bool] = False
-TESTING: type[bool] = False
-JWT_SECRET_KEY: type[str] = secrets.token_hex()
-SECRET_KEY: type[str] = secrets.token_hex()
-TEMPLATES_AUTO_RELOAD: type[bool] = False
+DEBUG = False
+TESTING = False
+JWT_SECRET_KEY = secrets.token_hex()
+SECRET_KEY = secrets.token_hex()
+TEMPLATES_AUTO_RELOAD = False
 
 # FLASK-MAIL CONFIG
-MAIL_SERVER: type[str] = ""
-MAIL_PORT: type[int] = 587
-MAIL_USE_TLS: type[bool] = False
-MAIL_USE_SSL: type[bool] = False
-MAIL_USERNAME: type[str] = ""
-MAIL_PASSWORD: type[str] = ""
-MAIL_DEFAULT_SENDER: type[str] = ""
+MAIL_SERVER = ""
+MAIL_PORT = 587
+MAIL_USE_TLS = False
+MAIL_USE_SSL = False
+MAIL_USERNAME = ""
+MAIL_PASSWORD = ""
+MAIL_DEFAULT_SENDER = ""
 
 # SQLALCHEMY CONFIG
-SQLALCHEMY_POOL_SIZE: type[int] = 30  # Número de conexões na pool
+SQLALCHEMY_POOL_SIZE = 30  # Número de conexões na pool
 
 # Número de conexões extras além da pool_size
-SQLALCHEMY_MAX_OVERFLOW: type[int] = 10
+SQLALCHEMY_MAX_OVERFLOW = 10
 
 # Tempo de espera para obter uma conexão
-SQLALCHEMY_POOL_TIMEOUT: type[int] = 30
+SQLALCHEMY_POOL_TIMEOUT = 30
 
 # Tempo (em segundos) para reciclar as conexões ociosas
-SQLALCHEMY_POOL_RECYCLE: type[int] = 1800
+SQLALCHEMY_POOL_RECYCLE = 1800
 
 # Verificar a saúde da conexão antes de usá-la
-SQLALCHEMY_POOL_PRE_PING: type[bool] = True
+SQLALCHEMY_POOL_PRE_PING = True
 
-SQLALCHEMY_DATABASE_URI: type[str] = "sqlite:///local.db"
-SQLALCHEMY_ENGINE_OPTIONS: dict[str, str | bool] = {"pool_pre_ping": True}
-SQLALCHEMY_TRACK_MODIFICATIONS: type[bool] = False
+SQLALCHEMY_DATABASE_URI = "sqlite:///local.db"
+SQLALCHEMY_ENGINE_OPTIONS = {"pool_pre_ping": True}
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-PERMANENT_SESSION_LIFETIME: type[int] = timedelta(days=31).max.seconds
+PERMANENT_SESSION_LIFETIME = timedelta(days=31).max.seconds
 
-REDIS_HOST: type[str] = ""
-REDIS_PORT: type[int] = 6379
-REDIS_DB: type[str] = ""
-REDIS_PASSWORD: type[str] = ""
+REDIS_HOST = ""
+REDIS_PORT = 6379
+REDIS_DB = ""
+REDIS_PASSWORD = ""
 
-BROKER_DATABASE: type[int] = 1
-RESULT_BACKEND_DATABASE: type[int] = 2
+BROKER_DATABASE = 1
+RESULT_BACKEND_DATABASE = 2
