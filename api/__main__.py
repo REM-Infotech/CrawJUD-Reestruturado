0d30a783cbc91f2676e0d1d3ed23a1595c8b0f75
Robotz213 <nicholas@robotz.dev>
2025-06-10 17:41:15 -0300

    refactor: update __main__.py to remove unused clear function
    refactor: update poetry.lock to bump openai and quart-socketio package versions

--- api/__main__.py
+++ api/__main__.py
@@ -2,14 +2,11 @@
 
 import asyncio
 
-from clear import clear
 from tqdm import tqdm
 
 from api._main import main_app
 
 if __name__ == "__main__":
-    clear()
-
     try:
         asyncio.run(main_app())
     except KeyboardInterrupt:
