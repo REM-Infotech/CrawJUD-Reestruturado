5a2fd6eef33719f32eda08601fea51e2ce2e2ae5
Robotz213 <nicholas@robotz.dev>
2025-07-10 18:10:35 -0400

    Add python-magic dependency to pyproject.toml

--- api/domains/file_service.py
+++ api/domains/file_service.py
@@ -1,10 +1,13 @@
 """Serviço de domínio para manipulação de arquivos e sessões."""
 
+from asyncio import iscoroutinefunction
 from typing import Any, AnyStr
 
 from anyio import Path
-from quart import request
+from quart import request, session
 from quart.datastructures import FileStorage
+from werkzeug.datastructures import MultiDict
+from werkzeug.datastructures.file_storage import FileStorage as WerkZeugFileStorage
 
 
 class FileService:
@@ -12,13 +15,20 @@ class FileService:
 
     async def save_file(self) -> None:
         """Salva um arquivo enviado para o diretório temporário especificado."""
-        file_data = await request.files
+        file_data: MultiDict[
+            str, FileStorage | WerkZeugFileStorage
+        ] = await request.files
         path_temp = (await Path(__file__).cwd()).joinpath(
-            "temp", file_data.get("id_temp")
+            "temp", session["uuid_temp_files"]
         )
+
+        await path_temp.mkdir(exist_ok=True, parents=True)
         for _, v in list(file_data.items()):
-            value: FileStorage = v
-            await value.save(path_temp)
+            is_coroutine = iscoroutinefunction(v.save)
+            if is_coroutine:
+                await v.save(path_temp.joinpath(v.filename))
+            elif not is_coroutine:
+                v.save(path_temp.joinpath(v.filename))
 
     async def save_session(
         self,
