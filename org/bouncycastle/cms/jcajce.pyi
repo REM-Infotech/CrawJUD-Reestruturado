3d2249a749c840a8e1f104eccaffcd8c4b351988
Robotz213 <nicholas@robotz.dev>
2025-07-19 21:01:59 +0000

    ✨ feat(java): adicionar novos protocolos e classes para integração
    Adiciona novas definições de protocolos e classes para suporte a operações de entrada/saída, manipulação de objetos e gerenciamento de chaves.
    
    Files Changed:
    - *java/__init__.pyi*: arquivo inicial criado.
    - *java/io.pyi*: definição de protocolos para FileDescriptor, FileChannel e FileInputStream.
    - *java/lang.pyi*: definição de protocolos para Object com métodos adicionais.
    - *java/security.pyi*: definição de protocolos para KeyStore e Signature.
    - *java/util.pyi*: definição de protocolos para ArrayList.
    - *org/__init__.pyi*: arquivo inicial criado.
    - *org/bouncycastle/__init__.pyi*: arquivo inicial criado.
    - *org/bouncycastle/cert/__init__.pyi*: arquivo inicial criado.
    - *org/bouncycastle/cert/jcajce.pyi*: definição de protocolos para JcaCertStore e JcaX509CertificateHolder.
    - *org/bouncycastle/cms/__init__.pyi*: definição de protocolos para CMSSignedDataGenerator e CMSProcessableByteArray.
    - *org/bouncycastle/cms/jcajce.pyi*: definição de protocolo para JcaSignerInfoGeneratorBuilder.
    - *org/bouncycastle/operator/__init__.pyi*: arquivo inicial criado.
    - *org/bouncycastle/operator/jcajce.pyi*: definição de protocolos para JcaDigestCalculatorProviderBuilder e JcaContentSignerBuilder.

--- org/bouncycastle/cms/jcajce.pyi
+++ org/bouncycastle/cms/jcajce.pyi
@@ -2,6 +2,3 @@ from typing import Protocol
 
 class JcaSignerInfoGeneratorBuilder(Protocol):
     def __init__(self) -> None: ...
-
-class JcaContentSignerBuilder(Protocol):
-    def __init__(self) -> None: ...
--- org/bouncycastle/operator/jcajce.pyi
+++ org/bouncycastle/operator/jcajce.pyi
@@ -2,3 +2,6 @@ from typing import Protocol
 
 class JcaDigestCalculatorProviderBuilder(Protocol):
     def __init__(self) -> None: ...
+
+class JcaContentSignerBuilder(Protocol):
+    def __init__(self) -> None: ...
