db9d196b2f83e45585e28cf2c10d627b35d0ddb5
Robotz213 <nicholas@robotz.dev>
2025-07-29 12:50:16 +0000

    ✨ *refact(bot): adicionar suporte a métodos OPTIONS na rota start_bot*
    Adiciona o método OPTIONS à rota start_bot e ao decorador crossdomain, permitindo melhor suporte a requisições CORS.
    
    Files Changed:
    - *api/routes/bot/__init__.py*: atualiza a rota start_bot para incluir o método OPTIONS.

--- api/routes/bot/__init__.py
+++ api/routes/bot/__init__.py
@@ -23,8 +23,8 @@ from api.wrapper import crossdomain
 bot = Blueprint("bot", __name__, url_prefix="/bot")
 
 
-@bot.route("/start_bot", methods=["get", "post"])
-@crossdomain(origin="*", methods=["get", "post"])
+@bot.route("/start_bot", methods=["get", "post", "options"])
+@crossdomain(origin="*", methods=["get", "post", "options"])
 @jwt_required
 async def start_bot() -> None:  # noqa: D103
     try:
