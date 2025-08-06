ac0e51b25fcf0d63e896cc0049f1a7381b335e38
Robotz213 <nicholas@robotz.dev>
2025-08-06 19:35:14 -0400

    ✨ refactor(capa): adicionar controle de concorrência com semáforo
    Implementa um semáforo para controlar a concorrência nas operações de download e processamento de dados, melhorando a eficiência e evitando sobrecarga no sistema.
    
    Files Changed:
    - *crawjud/bots/pje/capa.py*: adiciona semáforo para limitar a concorrência em processos e downloads.

--- crawjud/bots/pje/capa.py
+++ crawjud/bots/pje/capa.py
@@ -1,6 +1,7 @@
 # noqa: D100
 from __future__ import annotations
 
+import threading
 import traceback
 from concurrent.futures import Future, ThreadPoolExecutor
 from contextlib import suppress
@@ -125,6 +126,8 @@ class Capa(ClassBot, ContextTask):  # noqa: D101
 
         total_rows = len(bot_data)
 
+        semaforo = threading.Semaphore(4)
+
         with ThreadPoolExecutor(4) as executor:
             for regiao, data_regiao in list(regioes["regioes"].items()):
                 if self.stop_bot:
@@ -173,6 +176,7 @@ class Capa(ClassBot, ContextTask):  # noqa: D101
                         "start_time": start_time,
                         "total_rows": total_rows,
                         "position_process": position_process,
+                        "semaforo": semaforo,
                     })
 
                     task = executor.submit(self.queue_processo, **kw_args)
@@ -192,8 +196,9 @@ class Capa(ClassBot, ContextTask):  # noqa: D101
             with suppress(Exception):
                 task.result()
 
-    def queue_processo(
+    def queue_processo(  # noqa: D417
         self,
+        semaforo: threading.Semaphore,
         cookies: dict[str, str],
         headers: dict[str, str],
         base_url: str,
@@ -224,98 +229,111 @@ class Capa(ClassBot, ContextTask):  # noqa: D101
             None: Não retorna valor.
 
         """
+        try:
+            semaforo.acquire()
 
-        def print_erro() -> None:
-            self.print_msg(
-                pid=pid,
-                message="Falha ao obter informações do processo ",
-                row=row,
-                type_log="error",
-                total_rows=item.get("total_rows", 0),
-                start_time=start_time,
-            )
+            def print_erro() -> None:
+                self.print_msg(
+                    pid=pid,
+                    message="Falha ao obter informações do processo ",
+                    row=row,
+                    type_log="error",
+                    total_rows=item.get("total_rows", 0),
+                    start_time=start_time,
+                )
 
-        with ThreadPoolExecutor(4) as executor:
-            for item in data:
-                if self.stop_bot:
-                    break
-                try:
-                    # Atualiza dados do item para processamento
+            semaforo2 = threading.Semaphore(4)
 
-                    item.update({
-                        "row": position_process[item["NUMERO_PROCESSO"]] + 1,
-                        "total_rows": total_rows,
-                        "pid": pid,
-                        "url_base": base_url,
-                        "start_time": start_time,
-                    })
+            with ThreadPoolExecutor(4) as executor:
+                for item in data:
+                    if self.stop_bot:
+                        break
+                    try:
+                        # Atualiza dados do item para processamento
 
-                    row = int(item["row"])
-                    start_time = item["start_time"]
-                    resultados_busca: DictReturnDesafio = self.buscar_processo(
-                        data=item, headers=headers, cookies=cookies
-                    )
-
-                    # Verifica se houve resultado na busca
-
-                    # Verifica se resultados_busca é válido e possui dados
-                    results = (
-                        resultados_busca.get("results")
-                        if isinstance(resultados_busca, dict)
-                        else None
-                    )
-                    data_request = results.get("data_request") if results else None
-
-                    if not data_request:
-                        print_erro()
-                        continue
-
-                    # Salva dados em cache
-                    self.save_success_cache(
-                        data={
+                        item.update({
+                            "row": position_process[item["NUMERO_PROCESSO"]] + 1,
+                            "total_rows": total_rows,
                             "pid": pid,
-                            "data": resultados_busca["results"]["data_request"],
-                            "processo": item["NUMERO_PROCESSO"],
-                        }
-                    )
+                            "url_base": base_url,
+                            "start_time": start_time,
+                        })
 
-                    file_name = f"COPIA INTEGRAL {item['NUMERO_PROCESSO']} {pid}.pdf"
-                    _ft = executor.submit(
-                        self.copia_integral,
-                        pid=pid,
-                        data=item,
-                        total_rows=total_rows,
-                        row=row,
-                        url_base=base_url,
-                        file_name=file_name,
-                        headers=headers,
-                        start_time=start_time,
-                        cookies=cookies,
-                        id_processo=resultados_busca["results"]["id_processo"],
-                        captchatoken=resultados_busca["results"]["captchatoken"],
-                    )
+                        row = int(item["row"])
+                        start_time = item["start_time"]
+                        resultados_busca: DictReturnDesafio = self.buscar_processo(
+                            data=item, headers=headers, cookies=cookies
+                        )
 
-                    self.print_msg(
-                        pid=pid,
-                        message=f"Informações do processo {item['NUMERO_PROCESSO']} salvas com sucesso!",
-                        row=row,
-                        type_log="success",
-                        total_rows=item.get("total_rows", 0),
-                        start_time=start_time,
-                    )
+                        # Verifica se houve resultado na busca
 
-                except Exception as e:
-                    self.print_msg(
-                        message="\n".join(traceback.format_exception(e)),
-                        pid=pid,
-                        row=row,
-                        type_log="error",
-                        total_rows=item.get("total_rows", 0),
-                        start_time=start_time,
-                    )
+                        # Verifica se resultados_busca é válido e possui dados
+                        results = (
+                            resultados_busca.get("results")
+                            if isinstance(resultados_busca, dict)
+                            else None
+                        )
+                        data_request = (
+                            results.get("data_request") if results else None
+                        )
+
+                        if not data_request:
+                            print_erro()
+                            continue
+
+                        # Salva dados em cache
+                        self.save_success_cache(
+                            data={
+                                "pid": pid,
+                                "data": resultados_busca["results"]["data_request"],
+                                "processo": item["NUMERO_PROCESSO"],
+                            }
+                        )
+
+                        file_name = (
+                            f"COPIA INTEGRAL {item['NUMERO_PROCESSO']} {pid}.pdf"
+                        )
+                        _ft = executor.submit(
+                            self.copia_integral,
+                            semaforo=semaforo2,
+                            pid=pid,
+                            data=item,
+                            total_rows=total_rows,
+                            row=row,
+                            url_base=base_url,
+                            file_name=file_name,
+                            headers=headers,
+                            start_time=start_time,
+                            cookies=cookies,
+                            id_processo=resultados_busca["results"]["id_processo"],
+                            captchatoken=resultados_busca["results"]["captchatoken"],
+                        )
+
+                        self.print_msg(
+                            pid=pid,
+                            message=f"Informações do processo {item['NUMERO_PROCESSO']} salvas com sucesso!",
+                            row=row,
+                            type_log="success",
+                            total_rows=item.get("total_rows", 0),
+                            start_time=start_time,
+                        )
+
+                    except Exception as e:
+                        self.print_msg(
+                            message="\n".join(traceback.format_exception(e)),
+                            pid=pid,
+                            row=row,
+                            type_log="error",
+                            total_rows=item.get("total_rows", 0),
+                            start_time=start_time,
+                        )
+
+        finally:
+            semaforo.release()
 
     def copia_integral(  # noqa: D417
         self,
+        semaforo: threading.Semaphore,
         data: BotData,
         pid: str,
         row: int,
@@ -350,66 +368,71 @@ class Capa(ClassBot, ContextTask):  # noqa: D101
         """
         self.current_task = kwargs.get("current_task")
         storage = Storage("minio")
-        with Client(
-            base_url=url_base,
-            timeout=30,
-            headers=headers,
-            cookies=cookies,
-        ) as client:
-            try:
-                self.print_msg(
-                    message=f"Baixando arquivo do processo n.{data['NUMERO_PROCESSO']}",
-                    pid=pid,
-                    row=row,
-                    type_log="info",
-                    total_rows=total_rows,
-                    start_time=start_time,
-                )
-                response = client.get(
-                    url=f"/processos/{id_processo}/integra?tokenCaptcha={captchatoken}"
-                )
-                chunk = 65536
-                _path_temp = Path(__file__).cwd().joinpath("temp", pid)
 
-                _path_temp.mkdir(exist_ok=True, parents=True)
-
-                file_path = _path_temp.joinpath(file_name)
-                # Salva arquivo em chunks no storage
-                for pos, _bytes in enumerate(response.iter_bytes(chunk)):
-                    mode = "ab"
-                    if pos == 0:
-                        mode = "wb"
-
-                    with file_path.open(mode) as f:
-                        f.write(_bytes)
-
-                file_size = path.getsize(file_path)
-                dest_name = path.join(pid.upper(), file_name)
-
-                with file_path.open("rb") as file:
-                    storage.put_object(
-                        object_name=dest_name,
-                        data=file,
-                        length=file_size,
+        try:
+            semaforo.acquire()
+            with Client(
+                base_url=url_base,
+                timeout=30,
+                headers=headers,
+                cookies=cookies,
+            ) as client:
+                try:
+                    self.print_msg(
+                        message=f"Baixando arquivo do processo n.{data['NUMERO_PROCESSO']}",
+                        pid=pid,
+                        row=row,
+                        type_log="info",
+                        total_rows=total_rows,
+                        start_time=start_time,
                     )
+                    response = client.get(
+                        url=f"/processos/{id_processo}/integra?tokenCaptcha={captchatoken}"
+                    )
+                    chunk = 65536
+                    _path_temp = Path(__file__).cwd().joinpath("temp", pid)
 
-                self.print_msg(
-                    message=f"Arquivo do processo n.{data['NUMERO_PROCESSO']} baixado com sucesso!",
-                    pid=pid,
-                    row=row,
-                    type_log="success",
-                    total_rows=total_rows,
-                    start_time=start_time,
-                )
-            except Exception as e:
-                msg = "Erro ao baixar arquivo: " + "\n".join(
-                    traceback.format_exception(e)
-                )
-                self.print_msg(
-                    message=msg,
-                    pid=pid,
-                    row=row,
-                    type_log="error",
-                    total_rows=total_rows,
-                    start_time=start_time,
-                )
+                    _path_temp.mkdir(exist_ok=True, parents=True)
+
+                    file_path = _path_temp.joinpath(file_name)
+                    # Salva arquivo em chunks no storage
+                    for pos, _bytes in enumerate(response.iter_bytes(chunk)):
+                        mode = "ab"
+                        if pos == 0:
+                            mode = "wb"
+
+                        with file_path.open(mode) as f:
+                            f.write(_bytes)
+
+                    file_size = path.getsize(file_path)
+                    dest_name = path.join(pid.upper(), file_name)
+
+                    with file_path.open("rb") as file:
+                        storage.put_object(
+                            object_name=dest_name,
+                            data=file,
+                            length=file_size,
+                        )
+
+                    self.print_msg(
+                        message=f"Arquivo do processo n.{data['NUMERO_PROCESSO']} baixado com sucesso!",
+                        pid=pid,
+                        row=row,
+                        type_log="success",
+                        total_rows=total_rows,
+                        start_time=start_time,
+                    )
+                except Exception as e:
+                    msg = "Erro ao baixar arquivo: " + "\n".join(
+                        traceback.format_exception(e)
+                    )
+                    self.print_msg(
+                        message=msg,
+                        pid=pid,
+                        row=row,
+                        type_log="error",
+                        total_rows=total_rows,
+                        start_time=start_time,
+                    )
+        finally:
+            semaforo.release()
