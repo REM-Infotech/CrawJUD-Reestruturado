3ee04ecae6115a641b3f359c475b6b9cebc46921
Robotz213 <nicholas@robotz.dev>
2025-08-07 14:11:43 -0400

    ✨ feat(auth): implementar autenticação e busca de processos no sistema PJe
    Adiciona a classe PJeSearch para realizar buscas de processos e resolver captchas no sistema PJe. A classe utiliza a estrutura de controle de autenticação existente e integra tarefas assíncronas com Celery.
    
    Files Changed:
    - *crawjud/addons/auth/controller.py*: refatora a classe AuthController para suportar generics e remove métodos não utilizados.
    - *crawjud/addons/auth/pje.py*: cria a função autenticar para gerenciar a autenticação no sistema PJe.
    - *crawjud/addons/search/controller.py*: refatora a classe SearchController para suportar generics e remove métodos não utilizados.
    - *crawjud/addons/search/pje.py*: adiciona a classe PJeSearch com métodos para buscar processos e resolver captchas.

--- crawjud/addons/auth/pje.py
+++ crawjud/addons/auth/pje.py
@@ -1,14 +1,126 @@
 """Módulo de controle de autenticação Pje."""
 
+import os
 from contextlib import suppress
 from time import sleep
+from typing import cast
 
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.wait import WebDriverWait
 
+from celery_app._wrapper import shared_task
+from celery_app.custom._canvas import subtask
+from celery_app.types._celery._canvas import AsyncResult
 from crawjud.addons.auth import AuthController
+from crawjud.common.exceptions.bot import ExecutionError
+from crawjud.types.bot import DictReturnAuth, TReturnAuth
+from utils.webdriver import DriverBot
+
+
+def url_task(regiao: str, type_format: str) -> AsyncResult | None:  # noqa: D103
+    return subtask("pje.formata_url_pje").apply_async(
+        kwargs={"regiao": regiao, "type_format": type_format}
+    )
+
+
+@shared_task(name="pje.autenticador")
+def autenticar[T](regiao: str, *args: T, **kwargs: T) -> TReturnAuth:  # noqa: D417
+    r"""
+    Realiza a autenticação no sistema PJe utilizando certificado digital.
+
+    Args:
+        regiao (str): Região do tribunal para autenticação.
+
+    Returns:
+        (TReturnAuth | MessageTimeoutAutenticacao):
+            `TReturnAuth`: Dicionário contendo cookies, headers e base_url autenticados.
+
+            `MessageTimeoutAutenticacao`: Mensagem de erro caso o tempo de espera para validação de sessão seja excedido.
+
+    Raises:
+        TimeoutException: Caso ocorra um timeout ao procurar elementos na página.
+
+    """
+    try:
+        driver = DriverBot(
+            selected_browser="chrome",
+            with_proxy=True,
+        )
+        wait = driver.wait
+
+        url_login_task = url_task(regiao=regiao, type_format="login")
+        url_valida_sessao_task = url_task(regiao=regiao, type_format="validate_login")
+
+        url_login: str = url_login_task.wait_ready()
+        url_valida_sessao: str = url_valida_sessao_task.wait_ready()
+
+        driver.get(url_login)
+        btn_sso = wait.until(
+            ec.presence_of_element_located((
+                By.CSS_SELECTOR,
+                'button[id="btnSsoPdpj"]',
+            ))
+        )
+        btn_sso.click()
+
+        sleep(5)
+
+        btn_certificado = wait.until(
+            ec.presence_of_element_located((
+                By.CSS_SELECTOR,
+                ('div[class="certificado"] > a'),
+            ))
+        )
+        event_cert = btn_certificado.get_attribute("onclick")
+        driver.execute_script(event_cert)
+        sleep(1)
+        try:
+            WebDriverWait(driver, 15).until(ec.url_to_be(url_valida_sessao))
+        except TimeoutException:
+            if "pjekz" not in driver.current_url:
+                return "Tempo de espera excedido para validação de sessão"
+
+        if (
+            "pjekz/painel/usuario-externo" in driver.current_url
+            or "pjekz" in driver.current_url
+        ):
+            driver.refresh()
+
+        cookies_driver = driver.get_cookies()
+        _har_data = driver.current_HAR
+        entries = list(_har_data.entries)
+        entry_proxy = [
+            item
+            for item in entries
+            if f"https://pje.trt{regiao}.jus.br/pje-comum-api/" in item.request.url
+        ][-1]
+
+        _cookies = {
+            str(cookie["name"]): str(cookie["value"]) for cookie in cookies_driver
+        }
+
+        _headers = {
+            str(header["name"]): str(header["value"])
+            for header in entry_proxy.request.headers
+        }
+        driver.quit()
+
+        return cast(
+            DictReturnAuth,
+            {
+                "cookies": _cookies,
+                "headers": _headers,
+                "base_url": f"https://pje.trt{regiao}.jus.br/pje-consulta-api/api",
+            },
+        )
+
+    except Exception as e:
+        with suppress(Exception):
+            driver.quit()
+
+        raise ExecutionError(kwargs.get("pid", str(os.getpid())), e) from e
 
 
 class PjeAuth(AuthController):
@@ -16,40 +128,63 @@ class PjeAuth(AuthController):
 
     def auth(self) -> bool:  # noqa: D102
         try:
-            self.driver.get(self.elements.url_login)
+            task_autenticacao = subtask("pje.autenticador")
+            autenticacao_data: TReturnAuth = task_autenticacao.apply_async(
+                kwargs={"regiao": self.regiao}
+            ).wait_ready()
 
-            login = self.wait.until(
-                ec.presence_of_element_located((
-                    By.CSS_SELECTOR,
-                    self.elements.login_input,
-                ))
-            )
-            password = self.wait.until(
-                ec.presence_of_element_located((
-                    By.CSS_SELECTOR,
-                    self.elements.password_input,
-                ))
-            )
-            entrar = self.wait.until(
-                ec.presence_of_element_located((
-                    By.CSS_SELECTOR,
-                    self.elements.btn_entrar,
-                ))
-            )
+            if not isinstance(autenticacao_data, dict):
+                message_erro = "Erro ao realizar autenticação"
+                if str(autenticacao_data):
+                    message_erro = str(autenticacao_data)
 
-            login.send_keys(self.username)
-            sleep(0.5)
-            password.send_keys(self.password)
-            sleep(0.5)
-            entrar.click()
-
-            logado = None
-            with suppress(TimeoutException):
-                logado = WebDriverWait(self.driver, 10).until(
-                    ec.url_to_be(self.elements.chk_login)
+                self.print_msg(
+                    message=message_erro,
+                    type_log="error",
+                    errors=len(self.data_regiao),
                 )
+                return False
 
-            return logado is not None
+            self._cookies = autenticacao_data["cookies"]
+            self._base_url = autenticacao_data["base_url"]
+            self._headers = autenticacao_data["headers"]
+
+            return True
+
+            # self.driver.get(self.elements.url_login)
+
+            # login = self.wait.until(
+            #     ec.presence_of_element_located((
+            #         By.CSS_SELECTOR,
+            #         self.elements.login_input,
+            #     ))
+            # )
+            # password = self.wait.until(
+            #     ec.presence_of_element_located((
+            #         By.CSS_SELECTOR,
+            #         self.elements.password_input,
+            #     ))
+            # )
+            # entrar = self.wait.until(
+            #     ec.presence_of_element_located((
+            #         By.CSS_SELECTOR,
+            #         self.elements.btn_entrar,
+            #     ))
+            # )
+
+            # login.send_keys(self.username)
+            # sleep(0.5)
+            # password.send_keys(self.password)
+            # sleep(0.5)
+            # entrar.click()
+
+            # logado = None
+            # with suppress(TimeoutException):
+            #     logado = WebDriverWait(self.driver, 10).until(
+            #         ec.url_to_be(self.elements.chk_login)
+            #     )
+
+            # return logado is not None
 
         except Exception as e:
             raise e
--- crawjud/addons/search/controller.py
+++ crawjud/addons/search/controller.py
@@ -2,50 +2,17 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Self
+from abc import abstractmethod
 
-if TYPE_CHECKING:
-    from selenium.webdriver.remote.webdriver import WebDriver
-    from selenium.webdriver.support.wait import WebDriverWait
+from httpx import Client
 
-    from crawjud.types import BotData
-    from crawjud.types.elements import type_elements
-    from utils.printlogs import PrintMessage
+from crawjud.abstract._master import AbstractClassBot
+from crawjud.types.bot import BotData
+from crawjud.types.pje import DictResults
 
 
-class SearchController:
+class SearchController[T](AbstractClassBot):
     """Controller class for search operations."""
-
-    typebot: str
-    driver: WebDriver
-    wait: WebDriverWait
-    elements: type[type_elements]
-    bot_data: BotData
-    prt: PrintMessage
-
-    subclasses = {}
-
-    def __init__(
-        self,
-        typebot: str,
-        driver: WebDriver,
-        wait: WebDriverWait,
-        elements: type[type_elements],
-        bot_data: dict[str, str],
-        prt: PrintMessage,
-    ) -> None:
-        """Inicializador do SearchController."""
-        self.typebot = typebot
-        self.driver = driver
-        self.wait = wait
-        self.elements = elements
-        self.bot_data = bot_data
-        self.prt = prt
-
-    @classmethod
-    def construct(cls, system: str, *args, **kwargs) -> Self:  # noqa: ANN002, ANN003
-        """Método construtor para instanciar a classe."""
-        return cls.subclasses.get(system.lower())(*args, **kwargs)
 
     def __init_subclass__(cls) -> None:  # noqa: D105
         if not hasattr(cls, "search"):
@@ -53,13 +20,16 @@ class SearchController:
                 f"Subclasses of {cls.__name__} must implement the 'search' method."
             )
 
-        cls.subclasses[cls.__name__.replace("Search", "").lower()] = cls
+        cls.subclasses_search[cls.__name__.lower()] = cls
 
-    def search(self, bot_data: dict[str, str]) -> bool:  # noqa: D102
-        raise NotImplementedError("This method should be implemented by subclasses.")
+    @abstractmethod
+    def search(self, *args: T, **kwargs: T) -> T: ...  # noqa: D102
 
-    def search_proc(self) -> bool:  # noqa: D102
-        raise NotImplementedError("This method should be implemented by subclasses.")
-
-    def search_proc_parte(self) -> bool:  # noqa: D102
-        raise NotImplementedError("This method should be implemented by subclasses.")
+    def desafio_captcha(  # noqa: D102
+        self,
+        row: int,
+        data: BotData,
+        id_processo: str,
+        client: Client,
+    ) -> DictResults:
+        return NotImplementedError("Função não implementada!")
