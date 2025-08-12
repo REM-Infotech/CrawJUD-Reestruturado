c31208f750ba4fcbe82846117b43ff1776ab45e2
Robotz213 <nicholas@robotz.dev>
2025-08-12 13:20:43 -0400

    🔧 *refact(esaj): melhorar lógica de autenticação e simplificar código*
    Refatoração do módulo de autenticação Esaj para melhorar a lógica de autenticação e simplificar o código. Removidas exceções desnecessárias e reorganizadas as chamadas de métodos.
    
    Files Changed:
    - *crawjud_app/addons/auth/esaj.py*: refatoração da lógica de autenticação, remoção de exceções desnecessárias e simplificação do fluxo de login.

--- crawjud_app/addons/auth/esaj.py
+++ crawjud_app/addons/auth/esaj.py
@@ -2,9 +2,9 @@
 
 from __future__ import annotations
 
-import os
 import string
 from contextlib import suppress
+from pathlib import Path
 from time import sleep
 from typing import TYPE_CHECKING
 
@@ -23,161 +23,101 @@ class EsajAuth(AuthController):
     """Classe de autenticação Esaj."""
 
     def auth(self) -> bool:  # noqa: D102
-        try:
-            loginuser = "".join(
-                filter(lambda x: x not in string.punctuation, self.username),
+        loginuser = "".join(
+            filter(lambda x: x not in string.punctuation, self.username),
+        )
+        passuser = self.password
+        if self.login_method == "cert":
+            self.driver.get(self.elements.url_login_cert)
+            sleep(3)
+            loginopt: WebElement = self.wait.until(
+                ec.presence_of_element_located((
+                    By.CSS_SELECTOR,
+                    'select[id="certificados"]',
+                )),
             )
-            passuser = self.password
-            if self.login_method == "cert":
-                self.driver.get(self.elements.url_login_cert)
-                sleep(3)
-                loginopt: WebElement = self.wait.until(
-                    ec.presence_of_element_located((
+            loginopt = loginopt.find_elements(By.TAG_NAME, "option")
+
+            item = None
+
+            item = next(
+                filter(lambda item: loginuser in item.text, loginopt),
+                None,
+            )
+            if item:
+                sencert = item.get_attribute("value")
+                select = Select(
+                    self.driver.find_element(
                         By.CSS_SELECTOR,
                         'select[id="certificados"]',
-                    )),
+                    ),
                 )
-                loginopt = loginopt.find_elements(By.TAG_NAME, "option")
+                select.select_by_value(sencert)
+                entrar = self.driver.find_element(
+                    By.XPATH,
+                    '//*[@id="submitCertificado"]',
+                )
+                entrar.click()
+                sleep(2)
 
-                item = None
+                user_accept_cert_dir = Path(self.path_accepted) / "ACCEPTED"
+                if not user_accept_cert_dir.exists():
+                    self.accept_cert(user_accept_cert_dir)
 
-                try:
-                    item = next(
-                        filter(lambda item: loginuser in item.text, loginopt),
-                        None,
-                    )
-
-                except Exception as e:
-                    raise e
-                if item:
-                    try:
-                        sencert = item.get_attribute("value")
-                        select = Select(
-                            self.driver.find_element(
-                                By.CSS_SELECTOR,
-                                'select[id="certificados"]',
-                            ),
-                        )
-                        select.select_by_value(sencert)
-                        entrar = self.driver.find_element(
-                            By.XPATH,
-                            '//*[@id="submitCertificado"]',
-                        )
-                        entrar.click()
-                        sleep(2)
-
-                        user_accept_cert_dir = os.path.join(
-                            self.path_accepted,
-                            "ACCEPTED",
-                        )
-                        if not os.path.exists(user_accept_cert_dir):
-                            self.accept_cert(user_accept_cert_dir)
-
-                    except Exception as e:
-                        raise e
-
-                elif not item:
-                    return False
-
-                checkloged = None
-                with suppress(TimeoutException):
-                    checkloged = WebDriverWait(self.driver, 15).until(
-                        ec.presence_of_element_located(
-                            (
-                                By.CSS_SELECTOR,
-                                "#esajConteudoHome > table:nth-child(4) > tbody > tr > td.esajCelulaDescricaoServicos",
-                            ),
-                        ),
-                    )
-
-                if not checkloged:
-                    return False
-
-                return True
-
-            self.driver.get(self.elements.url_login)
-            sleep(3)
-
-            userlogin = self.driver.find_element(
-                By.CSS_SELECTOR,
-                self.elements.campo_username,
-            )
-            userlogin.click()
-            userlogin.send_keys(loginuser)
-
-            userpass = self.driver.find_element(
-                By.CSS_SELECTOR,
-                self.elements.campo_2_login,
-            )
-            userpass.click()
-            userpass.send_keys(passuser)
-            entrar = self.driver.find_element(
-                By.CSS_SELECTOR,
-                self.elements.btn_entrar,
-            )
-            entrar.click()
-            sleep(2)
+            elif not item:
+                return False
 
             checkloged = None
             with suppress(TimeoutException):
                 checkloged = WebDriverWait(self.driver, 15).until(
-                    ec.presence_of_element_located((
-                        By.CSS_SELECTOR,
-                        self.elements.chk_login,
-                    )),
+                    ec.presence_of_element_located(
+                        (
+                            By.CSS_SELECTOR,
+                            (
+                                "#esajConteudoHome > table:nth-child(4)",
+                                " > tbody > ",
+                                "tr > td.esajCelulaDescricaoServicos",
+                            ),
+                        ),
+                    ),
                 )
 
             return checkloged is not None
 
-        except Exception as e:
-            raise e
+        self.driver.get(self.elements.url_login)
+        sleep(3)
 
-    # def accept_cert(self, accepted_dir: str) -> None:
-    #     """Automatically accept a certificate using the certificate management tool.
+        userlogin = self.driver.find_element(
+            By.CSS_SELECTOR,
+            self.elements.campo_username,
+        )
+        userlogin.click()
+        userlogin.send_keys(loginuser)
 
-    #     Args:
-    #         accepted_dir (str): Directory path where accepted certificates are recorded.
+        userpass = self.driver.find_element(
+            By.CSS_SELECTOR,
+            self.elements.campo_2_login,
+        )
+        userpass.click()
+        userpass.send_keys(passuser)
+        entrar = self.driver.find_element(
+            By.CSS_SELECTOR,
+            self.elements.btn_entrar,
+        )
+        entrar.click()
+        sleep(2)
 
-    #     Executes certificate acceptance and copies necessary files.
+        checkloged = None
 
-    #     """
-    #     try:
-    #         path = r"C:\Users\%USERNAME%\AppData\Local\Softplan Sistemas\Web Signer"
-    #         resolved_path = os.path.expandvars(path)
+        try:
+            checkloged = WebDriverWait(self.driver, 15).until(
+                ec.presence_of_element_located((
+                    By.CSS_SELECTOR,
+                    self.elements.chk_login,
+                )),
+            )
+        except TimeoutException:
+            return False
 
-    #         app = Application(backend="uia").connect(path=resolved_path, cache_enable=True)
-    #         janela_principal = app.window()
-    #         janela_principal.set_focus()
-    #         button = janela_principal.descendants(control_type="Button")
-    #         checkbox = janela_principal.descendants(control_type="CheckBox")
-
-    #         sleep(0.5)
-
-    #         checkbox[0].click_input()
-    #         sleep(0.5)
-    #         button[1].click_input()
-
-    #         target_directory = Path(accepted_dir).parent.joinpath("chrome").resolve()
-
-    #         target_directory.mkdir(exist_ok=True)
-    #         source_directory = self.chr_dir
-
-    #         try:
-    #             comando = ["xcopy", source_directory, target_directory, "/E", "/H", "/C", "/I"]
-    #             resultados = subprocess.run(  # nosec: B603
-    #                 comando,
-    #                 check=True,
-    #                 text=True,
-    #                 stdout=subprocess.PIPE,
-    #                 stderr=subprocess.PIPE,
-    #             )
-    #             logger.info(str(resultados.stdout))
-
-    #         except Exception as e:
-    #             raise e
-
-    #         with open(Path(accepted_dir), "w", encoding="utf-8") as f:
-    #             f.write("")
-
-    #     except Exception as e:
-    #         raise e
+        else:
+            return checkloged is not None
