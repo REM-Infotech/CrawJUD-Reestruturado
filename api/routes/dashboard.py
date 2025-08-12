538a456272e57f877db05644ede34977d49a5617
Robotz213 <nicholas@robotz.dev>
2025-08-12 16:34:45 -0400

    🔧 *refact(dashboard): melhorar a estrutura e a legibilidade do código*
    Refatoração do módulo de rotas do dashboard para melhorar a legibilidade e a organização do código. As constantes de rótulos e meses executados foram movidas para o início do arquivo, eliminando duplicações e melhorando a clareza.
    
    Arquivos Alterados:
    - *api/routes/dashboard.py*: reorganização do código, remoção de duplicações e melhoria na legibilidade das funções de contagem e geração de datasets.

--- api/routes/dashboard.py
+++ api/routes/dashboard.py
@@ -1,5 +1,4 @@
-"""
-Module for dashboard routes.
+"""Module for dashboard routes.
 
 This module provides endpoints for rendering the dashboard and for serving
 chart data for executions per month and most executed bots.
@@ -23,12 +22,41 @@ if TYPE_CHECKING:
 
 dash = Blueprint("dash", __name__)
 
+LABELS = {
+    "1": "Janeiro",
+    "2": "Fevereiro",
+    "3": "Março",
+    "4": "Abril",
+    "5": "Maio",
+    "6": "Junho",
+    "7": "Julho",
+    "8": "Agosto",
+    "9": "Setembro",
+    "10": "Outubro",
+    "11": "Novembro",
+    "12": "Dezembro",
+}
 
-@dash.route("/linechart_system")
+MONTHS_EXECUTED = {
+    "Janeiro": 0,
+    "Fevereiro": 0,
+    "Março": 0,
+    "Abril": 0,
+    "Maio": 0,
+    "Junho": 0,
+    "Julho": 0,
+    "Agosto": 0,
+    "Setembro": 0,
+    "Outubro": 0,
+    "Novembro": 0,
+    "Dezembro": 0,
+}
+
+
+@dash.get("/linechart_system")
 @jwt_required
 async def linechart_system() -> Response:
-    """
-    Render the line chart page.
+    """Render the line chart page.
 
     Returns:
         Response: The response object containing the chart data.
@@ -36,21 +64,6 @@ async def linechart_system() -> Response:
     """
     try:
         executions = db.session.query(Executions).all()
-
-        labels = {
-            "1": "Janeiro",
-            "2": "Fevereiro",
-            "3": "Março",
-            "4": "Abril",
-            "5": "Maio",
-            "6": "Junho",
-            "7": "Julho",
-            "8": "Agosto",
-            "9": "Setembro",
-            "10": "Outubro",
-            "11": "Novembro",
-            "12": "Dezembro",
-        }
 
         contagem_execucoes = []
 
@@ -74,31 +87,18 @@ async def linechart_system() -> Response:
         }
 
         for system in ["PROJUDI", "PJE", "ESAJ", "ELAW", "CAIXA", "TJDF", "CAIXA"]:
-            executions_mes = {
-                "Janeiro": 0,
-                "Fevereiro": 0,
-                "Março": 0,
-                "Abril": 0,
-                "Maio": 0,
-                "Junho": 0,
-                "Julho": 0,
-                "Agosto": 0,
-                "Setembro": 0,
-                "Outubro": 0,
-                "Novembro": 0,
-                "Dezembro": 0,
-            }
+            executions_mes = MONTHS_EXECUTED.copy()
             for item in executions:
                 exec_bot: BotsCrawJUD = item.bot
 
                 if exec_bot.system.upper() == system:
-                    mes = labels[str(item.data_execucao.month)]
+                    mes = LABELS[str(item.data_execucao.month)]
                     executions_mes[mes] += 1
 
             contagem_execucoes.append({system: executions_mes})
 
         data: dict[str, dict[str, str] | list[dict[str, str]]] = {
-            "labels": list(labels.values()),
+            "labels": list(LABELS.values()),
             "datasets": [],
         }
 
@@ -109,10 +109,12 @@ async def linechart_system() -> Response:
                 numbers = list(value.values())
 
                 background_color = system_colors.get(key.upper(), {}).get(
-                    "background_color", "#000000"
+                    "background_color",
+                    "#000000",
                 )
                 border_color = system_colors.get(key.upper(), {}).get(
-                    "border_color", "#000000"
+                    "border_color",
+                    "#000000",
                 )
                 # Se não houver cor definida, gerar nova cor
                 if not system_colors.get(key.upper()):
@@ -140,36 +142,17 @@ async def linechart_system() -> Response:
         abort(500, "Erro ao gerar o gráfico de linha.")
 
 
-@dash.route("/linechart_bot")
+@dash.get("/linechart_bot")
 @jwt_required
 async def linechart_bot() -> Response:
-    """
-    Render the line chart page.
+    """Renderiza o gráfico de linha dos bots.
 
     Returns:
-        Response: The response object containing the chart data.
+        Response: Objeto de resposta com os dados do gráfico.
 
     """
     try:
         executions = db.session.query(Executions).all()
-
-        labels = {
-            "1": "Janeiro",
-            "2": "Fevereiro",
-            "3": "Março",
-            "4": "Abril",
-            "5": "Maio",
-            "6": "Junho",
-            "7": "Julho",
-            "8": "Agosto",
-            "9": "Setembro",
-            "10": "Outubro",
-            "11": "Novembro",
-            "12": "Dezembro",
-        }
-
-        contagem_execucoes = []
-
         system_colors = {
             "PROJUDI": {
                 "background_color": "#67277e",
@@ -188,76 +171,91 @@ async def linechart_bot() -> Response:
                 "border_color": "#925607",
             },
         }
-
         list_system: list[str] = []
-
+        # Identifique todos os bots únicos
         for item in executions:
             bot_exec: BotsCrawJUD = item.bot
             if bot_exec.display_name.upper() not in list_system:
                 list_system.append(item.bot.display_name.upper())
-
-        for bot in list_system:
-            executions_mes = {
-                "Janeiro": 0,
-                "Fevereiro": 0,
-                "Março": 0,
-                "Abril": 0,
-                "Maio": 0,
-                "Junho": 0,
-                "Julho": 0,
-                "Agosto": 0,
-                "Setembro": 0,
-                "Outubro": 0,
-                "Novembro": 0,
-                "Dezembro": 0,
-            }
-            for item in executions:
-                exec_bot: BotsCrawJUD = item.bot
-
-                if exec_bot.display_name.upper() == bot:
-                    mes = labels[str(item.data_execucao.month)]
-                    executions_mes[mes] += 1
-
-            contagem_execucoes.append({bot: executions_mes})
-
+        # Contabilize execuções por bot
+        contagem_execucoes = contar_execucoes_por_bot(executions, list_system)
+        # Gere datasets para o gráfico
+        datasets = gerar_dataset_bot(contagem_execucoes, system_colors)
         data: dict[str, dict[str, str] | list[dict[str, str]]] = {
-            "labels": list(labels.values()),
-            "datasets": [],
+            "labels": list(LABELS.values()),
+            "datasets": datasets,
         }
-
-        for contagem in contagem_execucoes:
-            it = contagem.items()
-
-            for key, value in it:
-                numbers = list(value.values())
-
-                background_color = system_colors.get(key.upper(), {}).get(
-                    "background_color", "#000000"
-                )
-                border_color = system_colors.get(key.upper(), {}).get(
-                    "border_color", "#000000"
-                )
-                # Se não houver cor definida, gerar nova cor
-                if not system_colors.get(key):
-                    # Gerar cor base
-                    r, g, b = gerar_cor_base()
-                    background_color = rgb_to_hex(r, g, b)
-
-                    # Gerar cor da borda mais escura
-                    r_borda, g_borda, b_borda = escurecer_cor(r, g, b)
-                    border_color = rgb_to_hex(r_borda, g_borda, b_borda)
-
-                setup_dataset = {
-                    "label": key,
-                    "data": numbers,
-                    "borderColor": border_color,
-                    "backgroundColor": background_color,
-                    "yAxisID": "y",
-                }
-
-                data["datasets"].append(setup_dataset)
-
         return await make_response(jsonify(dataset=data))
     except (KeyError, AttributeError, ValueError) as e:
         current_app.logger.error("\n".join(format_exception(e)))
         abort(500, "Erro ao gerar o gráfico de linha.")
+
+
+def contar_execucoes_por_bot(
+    executions: list[Executions],
+    bots: list[str],
+) -> list[dict[str, dict[str, int]]]:
+    """Contabilize execuções por bot para cada mês.
+
+    Args:
+        executions (list[Executions]): Lista de execuções.
+        bots (list[str]): Lista de nomes dos bots.
+
+    Returns:
+        list[dict[str, dict[str, int]]]: Lista de dicionários com execuções por bot.
+
+    """
+    contagem_execucoes = []
+    for bot in bots:
+        executions_mes = MONTHS_EXECUTED.copy()
+        for item in executions:
+            exec_bot: BotsCrawJUD = item.bot
+            if exec_bot.display_name.upper() == bot:
+                mes = LABELS[str(item.data_execucao.month)]
+                executions_mes[mes] += 1
+        contagem_execucoes.append({bot: executions_mes})
+    return contagem_execucoes
+
+
+def gerar_dataset_bot(
+    contagem_execucoes: list[dict[str, dict[str, int]]],
+    system_colors: dict[str, dict[str, str]],
+) -> list[dict[str, str | list[int]]]:
+    """Gere datasets para o gráfico de bots.
+
+    Args:
+        contagem_execucoes (list[dict[str, dict[str, int]]]): Contagem de
+            execuções por bot.
+        system_colors (dict[str, dict[str, str]]): Cores dos sistemas.
+
+    Returns:
+        list[dict[str, str | list[int]]]: Lista de datasets para o gráfico.
+
+    """
+    datasets = []
+    for contagem in contagem_execucoes:
+        for key, value in contagem.items():
+            numbers = list(value.values())
+            background_color = system_colors.get(key.upper(), {}).get(
+                "background_color",
+                "#000000",
+            )
+            border_color = system_colors.get(key.upper(), {}).get(
+                "border_color",
+                "#000000",
+            )
+            # Se não houver cor definida, gerar nova cor
+            if not system_colors.get(key.upper()):
+                r, g, b = gerar_cor_base()
+                background_color = rgb_to_hex(r, g, b)
+                r_borda, g_borda, b_borda = escurecer_cor(r, g, b)
+                border_color = rgb_to_hex(r_borda, g_borda, b_borda)
+            setup_dataset = {
+                "label": key,
+                "data": numbers,
+                "borderColor": border_color,
+                "backgroundColor": background_color,
+                "yAxisID": "y",
+            }
+            datasets.append(setup_dataset)
+    return datasets
