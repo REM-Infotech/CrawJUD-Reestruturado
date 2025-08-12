858e04fcc514fb30d48ce4ba51b99b679e190377
Robotz213 <nicholas@robotz.dev>
2025-08-12 11:39:42 -0400

    ✨ *feat(esaj): implementar módulos de busca, capa, e movimentação para o sistema CrawJUD-Bots*
    Adiciona novos módulos para gerenciar operações no sistema ESaj, incluindo busca de páginas, extração de informações de processos e movimentações.
    
    Files Changed:
    - *celery_app/bots/esaj/__init__.py*: Inicializa e configura o bot ESaj, incluindo novos componentes.
    - *celery_app/bots/esaj/busca_pags.py*: Implementa a lógica para busca de páginas e extração de custos.
    - *celery_app/bots/esaj/capa.py*: Gerencia operações de capa e extração de informações de processos.
    - *celery_app/bots/esaj/movimentacao.py*: Implementa a lógica para gerenciar e processar movimentações no sistema.

--- celery_app/bots/esaj/__init__.py
+++ celery_app/bots/esaj/__init__.py
@@ -8,12 +8,12 @@ import logging
 import traceback
 from typing import Callable, Union
 
+from celery_app.common.exceptions.bot import StartError
 from crawjud.bots.esaj.busca_pags import BuscaPags as Busca_pags
 from crawjud.bots.esaj.capa import Capa
 from crawjud.bots.esaj.emissao import Emissao
 from crawjud.bots.esaj.movimentacao import Movimentacao
 from crawjud.bots.esaj.protocolo import Protocolo
-from crawjud.common.exceptions.bot import StartError
 
 logger_ = logging.getLogger(__name__)
 
