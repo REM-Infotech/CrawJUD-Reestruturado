35295e56362bf01c3f0d2739a57e21c8bc6bb8fb
Robotz213 <nicholas@robotz.dev>
2025-08-12 17:40:06 -0400

    🔧 *refact: melhorar a configuração do logger do Celery*
    Refatoração da função de configuração do logger do Celery para melhorar a legibilidade e a estrutura do código. Removidos argumentos desnecessários e reorganizados parâmetros para maior clareza.
    
    Arquivos Alterados:
    - *crawjud_app/__init__.py*: removidos argumentos não utilizados na função `config_loggers` e reorganizados parâmetros para melhor legibilidade.

--- crawjud_app/__init__.py
+++ crawjud_app/__init__.py
@@ -6,7 +6,6 @@ import re
 from logging.config import dictConfig
 from os import environ, getenv
 from pathlib import Path
-from typing import AnyStr
 
 from celery.signals import after_setup_logger
 
@@ -19,24 +18,27 @@ app = Celery(__name__)
 
 @after_setup_logger.connect
 def config_loggers(
+    *,
     logger: logging.Logger,
-    *args: AnyStr,
-    **kwargs: AnyStr,
 ) -> None:
     """Configure and alter the Celery logger for the application.
 
-    This function updates the Celery logger configuration based on environment
-    variables and custom logging settings. It ensures that the Celery logger uses
-    the desired log level and handlers derived from the application's logging configuration.
+    This function updates the Celery logger
+    configuration based on environment variables
+    and custom logging settings. It ensures that
+    the Celery logger uses the desired log level
+    and handlers derived from the application's logging configuration.
 
     Args:
         logger (logging.Logger): The logger instance to configure.
         *args (AnyType): Positional arguments.
-        **kwargs (AnyType): Keyword arguments, may include a 'logger' instance to be configured.
+        **kwargs (AnyType): Keyword arguments, may include a
+            'logger' instance to be configured.
 
     """
     logger_name = environ.get(
-        "WORKER_NAME", str(re.sub(r"[^a-zA-Z0-9]", "_", "crawjud_app"))
+        "WORKER_NAME",
+        str(re.sub(r"[^a-zA-Z0-9]", "_", "crawjud_app")),
     )
     log_path = Path().cwd().joinpath("temp", "logs")
     log_path.mkdir(exist_ok=True, parents=True)
@@ -48,7 +50,9 @@ def config_loggers(
         log_level = logging.INFO
 
     config, _ = dict_config(
-        LOG_LEVEL=log_level, LOGGER_NAME=logger_name, FILELOG_PATH=log_file
+        LOG_LEVEL=log_level,
+        LOGGER_NAME=logger_name,
+        FILELOG_PATH=log_file,
     )
 
     logger.handlers.clear()
@@ -76,19 +80,7 @@ def make_celery() -> Celery:
 
     app.conf.update(config.celery_config)
 
-    # class ContextTask(celery.Task):
-    #     def __call__(
-    #         self,
-    #         *args: tuple,
-    #         **kwargs: dict,
-    #     ) -> any:  # -> any:
-    #         return self.run(*args, **kwargs)
-
-    # celery.Task = ContextTask
-
     app.conf.update(
-        # task_queues=CELERY_QUEUES,
-        # task_routes=CELERY_ROUTES,
         task_default_queue="default",
         task_default_exchange="default",
         task_default_routing_key="default",
