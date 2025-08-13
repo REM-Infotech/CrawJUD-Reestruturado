deb76ed3d723d715af50d3909ae8392370a3c0f2
Robotz213 <nicholas@robotz.dev>
2025-08-13 19:16:23 -0400

    🔧 *refact: Remover configuração de logger do Celery*
    Removida a configuração do logger do Celery para simplificar a inicialização do aplicativo. A remoção visa melhorar a clareza e a manutenção do código.
    
    Arquivos Alterados:
    - *crawjud_app/__init__.py*: Removido o código relacionado à configuração do logger do Celery, incluindo importações e a função de configuração.

--- crawjud_app/__init__.py
+++ crawjud_app/__init__.py
@@ -1,71 +1,11 @@
 """Módulo Celery App do CrawJUD Automatização."""
 
 import importlib
-import logging
-import re
-from contextlib import suppress
-from logging.config import dictConfig
-from os import environ, getenv
-from pathlib import Path
-
-from celery.signals import after_setup_logger
-from tqdm import tqdm
 
 from crawjud_app.custom import AsyncCelery as Celery
 from crawjud_app.resources.load_config import Config
-from utils.logger import dict_config
 
 app = Celery(__name__)
-
-with suppress(Exception):  # pragma: no cover
-
-    @after_setup_logger.connect
-    def config_loggers[T](
-        logger: logging.Logger,
-        **kwargs: T,
-    ) -> None:
-        """Configure and alter the Celery logger for the application.
-
-        This function updates the Celery logger
-        configuration based on environment variables
-        and custom logging settings. It ensures that
-        the Celery logger uses the desired log level
-        and handlers derived from the application's logging configuration.
-
-        Args:
-            logger (logging.Logger): The logger instance to configure.
-            *args (AnyType): Positional arguments.
-            **kwargs (AnyType): Keyword arguments, may include a
-                'logger' instance to be configured.
-
-        """
-        tqdm.write(str(kwargs))
-        logger_name = environ.get(
-            "WORKER_NAME",
-            str(re.sub(r"[^a-zA-Z0-9]", "_", "crawjud_app")),
-        )
-        log_path = Path().cwd().joinpath("temp", "logs")
-        log_path.mkdir(exist_ok=True, parents=True)
-        log_file = log_path.joinpath(f"{logger_name}.log")
-        log_file.touch(exist_ok=True)
-
-        log_level = logging.INFO
-        if getenv("DEBUG", "False").lower() == "true":
-            log_level = logging.DEBUG
-
-        config, _ = dict_config(
-            LOG_LEVEL=log_level,
-            LOGGER_NAME=logger_name,
-            FILELOG_PATH=log_file,
-        )
-
-        logger.handlers.clear()
-        dictConfig(config)
-        # Alter the Celery logger using the provided logger from kwargs if available.
-        logger.setLevel(log_level)
-        configured_logger = logging.getLogger(logger_name.replace("_", "."))
-        for handler in configured_logger.handlers:
-            logger.addHandler(handler)
 
 
 def make_celery() -> Celery:
