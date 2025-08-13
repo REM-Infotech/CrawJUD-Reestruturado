511a82ecd7cba1eb03cf7b883fd8f2a8161fc6a5
Robotz213 <nicholas@robotz.dev>
2025-08-13 19:05:05 -0400

    ✨ *chore: Adicionar configuração do ecosystem para o aplicativo*
    Adiciona um novo arquivo de configuração para o gerenciamento do aplicativo CrawJUD utilizando o PM2. Esta configuração permite a execução do Celery como um worker, com opções de reinício automático e monitoramento.
    
    Arquivos Alterados:
    - *ecosystem.config.js*: Novo arquivo criado com a configuração do aplicativo.

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: "crawjud_app",
+      cmd: "python -m celery_app",
+      args: "--type worker",
+      autorestart: true,
+      watch: true,
+      pid: "./.log/crawjud_app.pid",
+      // max_memory_restart: '1G',
+      // env: {
+      //   ENV: 'development'
+      // },
+      // env_production : {
+      //   ENV: 'production'
+      // }
+    },
+  ],
+};
