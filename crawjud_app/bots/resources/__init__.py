8a4bfd4e50d02df4493b431f2538ab7508fd7d31
Robotz213 <nicholas@robotz.dev>
2025-08-15 15:45:15 -0400

    🔧 *refact: Remover documentação de recursos dos bots*
    Removido o arquivo de documentação que fornecia informações sobre os recursos e utilitários utilizados pelos bots do sistema CrawJUD, pois não é mais necessário.
    
    Arquivos Alterados:
    - *crawjud_app/bots/resources/__init__.py*: arquivo de documentação removido.

--- crawjud_app/bots/resources/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Fornece recursos e utilitários para os bots do sistema CrawJUD.
-
-Este pacote contém módulos e funções auxiliares utilizados pelos bots
-para realizar operações específicas no contexto do projeto CrawJUD.
-"""
